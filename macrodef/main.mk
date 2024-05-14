c143ed5dfc31c10e9fff5401e165e5441f09cb86
Miu Razvan <miu.razvan28@gmail.com>
2024-05-14 14:04:23 +0300

    Update github actions

--- macrodef/main.mk
+++ macrodef/main.mk
@@ -27,32 +27,8 @@ docker-run:
 docker-shell:
 	@docker run -it -p 3000:3000 --entrypoint bash mlf-frontend:latest
 
-# .PHONY: sb
-# sb:		## start storybook in development mode
-# 	$(NPM) run storybook
-#
-# .PHONY: start
-# start:		## Start the website frontend in development mode
-# 	$(NPM) run dev
-
-# .PHONY: build
-# build:		## Build a production bundle
-# 	$(NPM) run build
-#
-# .next/BUILD_ID:
-# 	$(NPM) run build
-#
-# .PHONY: start-prod
-# start-prod:	.next/BUILD_ID		## Starts the frontend in production mode
-# 	$(NPM) run start
-
 .PHONY: test
 test:		## Run Jest tests
 	$(NPM) run test
 
-# .PHONY: clean
-# clean:		## Cleanup nextjs build folder
-# 	@echo "This will clean the .next folder"
-# 	rm -rf ./.next
-# 	@echo "Done"
 
